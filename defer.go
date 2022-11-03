1df23ac7ddbdfc38c573521ff83e4e5c450ad9ec
Daniel <dani2014games@gmail.com>
2022-11-03 14:37:04 -0300

    Versão 03/11

--- defer.go
+++ defer.go
@@ -1,38 +1,38 @@
 package main
 
-import (
+import ( // import the os package
     "fmt"
     "os"
 )
 
-func main() {
+func main() { // main function
 
-    f := createFile("/tmp/defer.txt")
-    defer closeFile(f)
-    writeFile(f)
+    f := createFile("/tmp/defer.txt") // create a file
+    defer closeFile(f) 			  // defer the closing of the file
+    writeFile(f) 				  // write to the file
 }
 
-func createFile(p string) *os.File {
-    fmt.Println("creating")
-    f, err := os.Create(p)
-    if err != nil {
-        panic(err)
+func createFile(p string) *os.File { // create a file
+    fmt.Println("creating") // print creating
+    f, err := os.Create(p)  // create the file
+    if err != nil { 	   // if there is an error
+        panic(err) 		   // panic
     }
-    return f
+    return f 			   // return the file
 }
 
-func writeFile(f *os.File) {
+func writeFile(f *os.File) { // write to the file
     fmt.Println("writing")
     fmt.Fprintln(f, "data")
 
 }
 
-func closeFile(f *os.File) {
-    fmt.Println("closing")
-    err := f.Close()
+func closeFile(f *os.File) { // close the file
+    fmt.Println("closing") // print closing
+    err := f.Close() 	   // close the file
 
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "error: %v\n", err)
-        os.Exit(1)
+    if err != nil { 	   // if there is an error
+        fmt.Fprintf(os.Stderr, "error: %v\n", err) // print the error
+        os.Exit(1) 							   // exit with a status of 1
     }
 }
